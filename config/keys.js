84cbfeb61c3e00dbbc95c1babdf341a18fd3236f
Reid <rmcc0724@gmail.com>
2019-05-14 11:38:26 -0700

    Mongo DB connected

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-mongoURI: 'mongodb://rmcc0724:Starbuck5@mern-shopping-ck3f0.mongodb.net/test?retryWrites=true'
+mongoURI: 'mongodb+srv://rmcc0724:6QMyRASAh6lJiLiz@mernshopping-ck3f0.mongodb.net/test?retryWrites=true'
 }
