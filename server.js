5c1ec3aa934687ac603abaf067a1a2e15e56616d
Reid <rmcc0724@gmail.com>
2019-05-15 12:29:57 -0700

    Error with parser fixed

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+//Import express for data models
+//Mongoose for our actual database
+//Bodyparser for the main app
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -7,15 +11,18 @@ const app = express();
 app.use(bodyParser.json());
 
 
-//DB config
+//DB config gets the URL from  keys.js for our mongo DB
 const db = require('./config/keys').mongoURI;
 
 //Connect to Mongo
 mongoose
-    .connect(db)
+//Connect to the database
+    .connect(db, { useNewUrlParser: true })
+//IF the connection is successful log the message below
     .then(() => console.log("MongoDB connected"))
+//IF there's an error log the error message
     .catch(err=> console.log('Errors: ' + err));
-
+//Set the server port to 5000
     const port = process.env.PORT || 5000;
-
+//Tell the server to start on port 5000
     app.listen(port, () => console.log(`Server has started on port ${port}`));
